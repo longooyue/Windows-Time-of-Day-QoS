1c3ee6f7a1e4734f0e365c9e46bdf9e56ca4d8d0
longooyue <longooyue@gmail.com>
2020-06-19 10:11:52 +0800

    Add files via upload

---
+++ LocalGPO_NetworkQoSTimeShift/ApplytoDay.bat
@@ -0,0 +1,2 @@
+xcopy .\day\Registry.pol C:\Windows\System32\GroupPolicy\Machine /E /H /K /Y
+gpupdate /force
---
+++ LocalGPO_NetworkQoSTimeShift/ApplytoNight.bat
@@ -0,0 +1,2 @@
+xcopy .\Night\Registry.pol C:\Windows\System32\GroupPolicy\Machine /E /H /K /Y
+gpupdate /force
